5da25e3f2293a419ae80e92a2cf9d7bab7827866
Loris <loris4526@gmail.com>
2017-05-25 22:14:44 +0200

    Add 'fetchWeather' action generator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,13 @@
 const API_KEY = "50d4cca3f04503d95851ca414e203a0e";
- 
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city){
+  const url = `ROOT_URL&q=${city}`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
