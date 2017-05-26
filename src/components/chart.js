ba6aeb27fa9450601e17753e2162f40b801790ae
Loris <loris4526@gmail.com>
2017-05-26 10:47:44 +0200

    Add charts for temps, pressures and humidities

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import {Sparklines,SparklinesLine} from "react-sparklines";
-
+import {Sparklines,SparklinesLine,SparklinesReferenceLine} from "react-sparklines";
+import _ from "lodash";
+function average(data){
+  return _.round(_.sum(data)/data.length);
+}
 
 export default (props)=>{
   return (
     <div>
       <Sparklines
         data={props.data}
-        width={180}
-        height={120}>
+        svgWidth={180}
+        svgHeight={120}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   )
 }
